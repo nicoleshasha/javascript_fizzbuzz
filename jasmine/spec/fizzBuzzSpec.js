35d5b4ba240653e276e15d987c67c34140efaceb
Ed Powderham <edpowderham@outlook.com>
2016-10-24 17:04:10 +0100

    Working game

--- jasmine/spec/fizzBuzzSpec.js
+++ jasmine/spec/fizzBuzzSpec.js
@@ -1,11 +1,35 @@
 describe("fizzBuzz", function() {
   var fizzBuzz;
 
-describe('knows when a number is', function(){
-  it('divisible by 3', function(){
-    fizzBuzz = new FizzBuzz();
-    expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
-  });
-});
+  describe('knows when a number is', function(){
+    it('divisible by 3', function(){
+      fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
+    });
 
+    it('not divisible by 3', function(){
+      fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByThree(5)).toBe(false);
+    });
+
+    it('divisible by 5', function(){
+      fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    it('not divisible by 5', function(){
+      fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByFive(3)).toBe(false);
+    });
+
+    it('is divisible by 15', function(){
+      fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
+    it('not divisible by 15', function(){
+      fizzBuzz = new FizzBuzz();
+      expect(fizzBuzz.isDivisibleByFifteen(16)).toBe(false);
+    });
+  });
 });
--- jasmine/src/fizzBuzz.js
+++ jasmine/src/fizzBuzz.js
@@ -1,7 +1,24 @@
-
-
 var FizzBuzz = function() {
 }
 FizzBuzz.prototype.isDivisibleByThree = function(number) {
-  return true;
+  return (number % 3 === 0);
+};
+FizzBuzz.prototype.isDivisibleByFive = function(number) {
+  return (number % 5 === 0);
+};
+FizzBuzz.prototype.isDivisibleByFifteen = function(number) {
+  return (number % 15 === 0);
+};
+
+FizzBuzz.prototype.return = function (number) {
+  if (this.isDivisibleByFifteen(number)){
+    return "FizzBuzz";
+  }
+  if (this.isDivisibleByThree(number)){
+    return "Fizz";
+  }
+  if (this.isDivisibleByFive(number)){
+    return "Buzz";
+  }
+  return number;
 };
